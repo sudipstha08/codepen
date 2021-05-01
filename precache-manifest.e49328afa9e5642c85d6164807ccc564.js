56ac49233aeb7fcf0d30d4af7aa29fcd4646ee21
Sudip Shrestha <sudipstha08@gmail.com>
2021-05-01 21:02:36 +0545

    Updates

---
+++ precache-manifest.e49328afa9e5642c85d6164807ccc564.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce00188a6870fba3a996bb0e3dba4ef",
+    "url": "/codepen/index.html"
+  },
+  {
+    "revision": "634fe4da50980fdd3491",
+    "url": "/codepen/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "dbfd6ff3062c4e11cdb0",
+    "url": "/codepen/static/css/main.fc3a185e.chunk.css"
+  },
+  {
+    "revision": "634fe4da50980fdd3491",
+    "url": "/codepen/static/js/2.54d50f37.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/codepen/static/js/2.54d50f37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbfd6ff3062c4e11cdb0",
+    "url": "/codepen/static/js/main.ea333cb2.chunk.js"
+  },
+  {
+    "revision": "f9ee33409a7c42d90d46",
+    "url": "/codepen/static/js/runtime-main.3a53c1ee.js"
+  },
+  {
+    "revision": "47ce9d1c87276dd11397396fe777e52f",
+    "url": "/codepen/static/media/cloud.47ce9d1c.svg"
+  },
+  {
+    "revision": "8a128b275fe26fcb9e78228267a40f1a",
+    "url": "/codepen/static/media/down-arrow.8a128b27.svg"
+  },
+  {
+    "revision": "d7f89524969d1faae0f232714a165845",
+    "url": "/codepen/static/media/logo.d7f89524.svg"
+  },
+  {
+    "revision": "9d5c7a8c742a3fbf81b1e41b14ae8f78",
+    "url": "/codepen/static/media/nerd.9d5c7a8c.svg"
+  },
+  {
+    "revision": "367027a8660c8c0e6665c012435b9a44",
+    "url": "/codepen/static/media/pen.367027a8.svg"
+  },
+  {
+    "revision": "213528bf4e9e1359c268b865931c142c",
+    "url": "/codepen/static/media/pinned.213528bf.svg"
+  },
+  {
+    "revision": "473a0fa210a98b0808dbbf43701a524a",
+    "url": "/codepen/static/media/settings.473a0fa2.svg"
+  },
+  {
+    "revision": "a3f7296228073ef8bd75a57147adda64",
+    "url": "/codepen/static/media/view-change.a3f72962.svg"
+  }
+]);
